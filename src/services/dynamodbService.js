335b3cc12200e3cf2f4ef4efe8c7b09537150460
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-07-11 20:28:38 -0300

    adding verifications

--- src/services/dynamodbService.js
+++ src/services/dynamodbService.js
@@ -1,27 +1,36 @@
 const { DynamoDBClient, DeleteItemCommand, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 
 exports.DeleteBlogPost = async function (key) {
+    if (!key) {
+        throw new Error('key is required')
+    }
+    else {
+        const client = new DynamoDBClient({ region: process.env.Region });
+        const command = new DeleteItemCommand({
+            TableName: process.env.BlogPostTableName,
+            Key: {
+                "title": key
+            }
+        });
 
-    const client = new DynamoDBClient({ region: process.env.Region });
-    const command = new DeleteItemCommand({
-        TableName: process.env.BlogPostTableName,
-        Key: {
-            "title": key
-        }
-    });
-
-    await client.send(command);
+        await client.send(command);
+    }
 }
 
 exports.GetBlogPostByKey = async function (key) {
-    const client = new DynamoDBClient({ region: process.env.Region });
-    const command = new GetItemCommand({
-        TableName: process.env.BlogPostTableName,
-        Key: {
-            'title': { S: key }
-        }
-    });
-    const response = await client.send(command);
+    if (!key) {
+        throw new Error('key is required')
+    }
+    else {
+        const client = new DynamoDBClient({ region: process.env.Region });
+        const command = new GetItemCommand({
+            TableName: process.env.BlogPostTableName,
+            Key: {
+                'title': { S: key }
+            }
+        });
+        const response = await client.send(command);
 
-    return response.Item;
+        return response.Item;
+    }
 }
